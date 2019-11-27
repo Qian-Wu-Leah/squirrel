5df9c5606d12a2e3a8fa1ce19badb834680dcb15
Qian <qwu0101@gmail.com>
2019-11-27 21:45:00 +0000

    try to migrate Models.py

--- project/squirrel_tracker/models.py
+++ project/squirrel_tracker/models.py
@@ -3,16 +3,16 @@ from django.db import models
 from django.utils.translation import gettext as _
 # Create your models here.
 
-class Sightings(modelmodel):
-    Latitude = models.FloatField(
+class Sightings(models.Model):
+    latitude = models.FloatField(
             max_length = 100,
             help_text=_('Latitude'),
             )
-    Longitude = models.FloatField(
+    longitude = models.FloatField(
         max_length = 100,
         help_text=_('Longitude'),
         )
-    Unique_Squirrel_ID = CharField(
+    unique_Squirrel_ID = models.CharField(
             max_length = 50,
             help_text=_('Squirrel_ID'),
             )
@@ -25,18 +25,131 @@ class Sightings(modelmodel):
             (AM,'AM'),
             (OTHER,'Other'),
             )
-    Shift = model.CharField(
+    shift = models.CharField(
             max_length=3,
             choices= SHIFT_CHOICES,
             default = OTHER,
             )
 
-    Date = models.DateField(
-            help_text=_('Bday of pet'),
+    date = models.DateField(
+            help_text=_('Date you saw it'),
             )
 
-    Running = model.BooleanField(
+    ADULT ='adult'
+    JUVENILE = 'juvenile'
+    UNKNOWN = 'unknown'
+    AGE_CHOICE=(
+            (ADULT,'Adult'),
+            (JUVENILE,'Juvenile'),
+            (UNKNOWN,'Unknown or Blank'),
+            )
+    age = models.CharField(
+        max_length = 20,
+        choices = AGE_CHOICE,
+        default = UNKNOWN,
+        )
+
+    BLACK='black'
+    CINNAMON= 'cinamon'
+    GRAY = 'gray'
+    PRIMARY_FUR_COLOR_CHOICE=(
+            (BLACK,'Black'),
+            (CINNAMON,'Cinnamon'),
+            (GRAY,'Gray'),
+            (OTHER,'Unknown or Other'),
+            )
+    primary_fur_color=models.CharField(
+        max_length = 20,
+        choices = PRIMARY_FUR_COLOR_CHOICE,
+        default = OTHER,
+        )
+
+
+    ABOVE_GROUND='above ground'
+    GROUND_PLANE='ground plane'
+    LOCATION_CHOICE=(
+        (ABOVE_GROUND,'Above Ground'),
+        (GROUND_PLANE,'Ground Plane'),
+        (OTHER,'Other'),
+        )
+    location = models.CharField(
+        max_length = 50,
+        choices = LOCATION_CHOICE,
+        default = OTHER,
+        )
+
+
+    specific_location = models.CharField(
+        max_length = 1000,
+	help_text=_('Specific Location'),
+	)
+
+    running = models.BooleanField(
             default = False,
             help_text = _('Whether running?'),
             )
+    chasing= models.BooleanField(
+            default = False,
+            help_text = _('Whether chasing?'),
+            )
+
+    climbing= models.BooleanField(
+            default = False,
+            help_text = _('Whether climbing?'),
+            )
+
+    eating= models.BooleanField(
+            default = False,
+            help_text = _('Whether eating?'),
+            )
+
+    foraging= models.BooleanField(
+            default = False,
+            help_text = _('Whether foraging?'),
+            )
+
+    other_activities = models.CharField(
+        max_length = 1000,
+        help_text=_('Other Activities'),
+        )
+
+    kuks = models.BooleanField(
+            default = False,
+            help_text = _('Whether Kuks?'),
+            )
+ 
+    quaas = models.BooleanField(
+            default = False,
+            help_text = _('Whether Quaas?'),
+            )
+
+    moans = models.BooleanField(
+            default = False,
+            help_text = _('Whether Moans?'),
+            )
+
+    tail_flags = models.BooleanField(
+                 default = False,
+                 help_text = _('Whether Tail Flags?'),
+                 )
+
+    tail_twitches = models.BooleanField(
+                    default = False,
+                    help_text = _('Whether Tail Twitches?'),
+                    )
+
+    approaches = models.BooleanField(
+                default = False,
+                help_text = _('Whether Approaches?'),
+                )
+
+    indifferent = models.BooleanField(
+                  default = False,
+                  help_text = _('Whether indifferent?'),
+                  )  
+
+    runs_from = models.BooleanField(
+                default = False,
+                help_text = _('Whether run from?'),
+                )
 
