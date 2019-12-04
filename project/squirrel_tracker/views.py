a53ecb21e7f71f79c2b1ed3f22b5c96829f5972b
Qian <qwu0101@gmail.com>
2019-12-04 00:34:49 +0000

    Sigghting Function and Add Function

--- project/squirrel_tracker/views.py
+++ project/squirrel_tracker/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Sighting
 
+from django.forms import ModelForm
+
+class SightingForm(ModelForm):
+    class Meta:
+        model = Sighting
+        fields = '__all__'
 
 def map(request):
     sightings = Sighting.objects.all()
@@ -10,14 +16,26 @@ def map(request):
             }
     return render(request,'squirrel_tracker/map.html',context)
 
+
 def sightings(request):
-    return render(request,'squirrel_tracker/sightings.html',context={'squirrels':Sighting.objects.all})
+    sightings = Sighting.objects.all()
+    context = {
+        'sightings':sightings, 
+        }
+    return render(request,'squirrel_tracker/sightings.html',context)
 
 def stats(request):
     return HttpResponse(f"STATS")
 
-def add(request):
-    return HttpResponse(f"ADD")
+def add(request): 
+    if request.method == 'POST':
+        form = SightingForm(request.POST)
+        if form.is_valid():           
+            form.save()
+            return redirect('sightings/')
+    else:
+        form = SightingForm()
+    return render(request, 'squirrel_tracker/add.html', {'form': form})
 
 def ID(request,squirrel_ID):
     squirrel = Sighting.objects.get(unique_squirrel_ID = squirrel_ID)
