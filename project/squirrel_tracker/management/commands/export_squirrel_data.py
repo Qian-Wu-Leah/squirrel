0d13fdc37d0a567731d967642c09531ad51dbf94
Joanne Tho <j.tho@columbia.edu>
2019-12-04 01:42:18 +0000

    Finish export_squirrel_data management command
    Ran command and created export_test.csv

--- project/squirrel_tracker/management/commands/export_squirrel_data.py
+++ project/squirrel_tracker/management/commands/export_squirrel_data.py
@@ -1,20 +1,23 @@
 from django.core.management.base import BaseCommand, CommandError
 from squirrel_tracker.models import Sighting
+from django.http import HttpResponse
+
 import csv
 import sys
 
 class Command(BaseCommand):
     help = ("Output Sightings as csv")
-   
 
     def add_arguments(self, parser):
         parser.add_argument('path', type=str)
 
     def handle(self, *args, **kwargs):
         path = kwargs['path']
-        from django.db.models import get_model
-        squirrel_tracker, Sighting = 
+        field_names = [f.name for f in Sighting._meta.fields]
 
+        with open(path, 'w') as csvf:
+            writer = csv.writer(csvf)
+            writer.writerow(field_names)
+            for instance in Sighting.objects.all():
+                writer.writerow([getattr(instance, f) for f in field_names])
 
-
-    
